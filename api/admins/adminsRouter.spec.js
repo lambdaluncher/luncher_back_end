15d9156b8423069e6ca9c57437a7846366a9a381
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-02-13 16:08:19 -0700

    Added files for helpers, routers, and router tests, and also configured index file to use routers and middleware

---
+++ api/admins/adminsRouter.spec.js
@@ -0,0 +1,6 @@
+const request = require('supertest');
+const admins = require('./adminsRouter.js');
+
+describe('The Admins route handlers', () => {
+
+});
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const adminsRouter = require('./api/admins/adminsRouter.js');
+const donorsRouter = require('./api/donors/donorsRouter.js');
+const schoolsRouter = require('./api/schools/schoolsRouter.js');
+
+const PORT = 4040; 
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+// server.use('/admins', adminsRouter);
+// server.use('/donors', donorsRouter);
+// server.use('/schools', schoolsRouter);
+
+server.listen(PORT, err => {
+    console.log(`Server is running on port ${PORT}`);
+});
