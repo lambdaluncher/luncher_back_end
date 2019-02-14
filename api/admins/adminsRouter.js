3c0be4b17d7fc1d5a3888a90be6b5d5755c7746b
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-02-14 14:56:17 -0700

    Added function to generate json web token for use in endpoints

--- api/admins/adminsRouter.js
+++ api/admins/adminsRouter.js
@@ -2,9 +2,21 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const { jwtKey } = require('../../auth/authenticate.js');
+
 const db = require('./adminsHelpers.js');
 
 const adminsRouter = express.Router();
+
+function generateToken(admin) {
+    const payload = {
+        username: admin.username
+    };
+    const options = {
+        expiresIn: '1h'
+    };
+    return jwt.sign(payload, jwtKey, options);
+}
 
 adminsRouter.get('/', async (req, res) => {
     const rows = await db.getAll();
