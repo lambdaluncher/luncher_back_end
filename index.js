75d91be30f1322ce0713d9b7a0a395a31be9bfd8
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-02-13 17:04:19 -0700

    Added seeds to test database

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const adminsRouter = require('./api/admins/adminsRouter.js');
 const donorsRouter = require('./api/donors/donorsRouter.js');
 const schoolsRouter = require('./api/schools/schoolsRouter.js');
 
-const PORT = 4040; 
+const port = process.env.PORT || 4040; 
 const server = express();
 
 server.use(cors());
@@ -16,6 +16,6 @@ server.use('/admins', adminsRouter);
 server.use('/donors', donorsRouter);
 server.use('/schools', schoolsRouter);
 
-server.listen(PORT, err => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(port, err => {
+    console.log(`Server is running on port ${port}`);
 });
