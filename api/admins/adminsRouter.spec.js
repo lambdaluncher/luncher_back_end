e0f534caed6fee503bf208d592fb9a551b4cbea1
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-02-13 18:37:01 -0700

    Added tests for get endpoint for Admins route handlers

--- api/admins/adminsRouter.spec.js
+++ api/admins/adminsRouter.spec.js
@@ -3,5 +3,27 @@ const admins = require('./adminsRouter.js');
 const db = require('../../data/dbConfig.js');
 
 describe('The Admins route handlers', () => {
+    afterEach(async () => {
+        await db('admins').truncate();
+    });
 
+    describe('get /', () => {
+        it('responds with status code 200', async () => {
+            const response = await request(admins).get('/');
+
+            expect(response.status).toBe(200);
+        });
+
+        it('responds with json type', async () => {
+            const response = await request(admins).get('/');
+
+            expect(response.type).toMatch(/json/i);
+        });
+
+        it('sends correct response object', async () => {
+            const response = await request(admins).get('/');
+
+            expect(response.body).toEqual([]);
+        });
+    });
 });
