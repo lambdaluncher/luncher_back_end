1e9356ee510bde3590a63786d9d398378e98918d
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-02-16 15:14:06 -0700

    Updates to admins router tests for consistency

--- api/admins/adminsRouter.spec.js
+++ api/admins/adminsRouter.spec.js
@@ -29,43 +29,42 @@ describe('The Admins route handlers', () => {
 
     describe('get /:id', () => {
         it('responds with status code 200', async () => {
+            await request(server).post('/admins/register').send({username: 'username', password: 'password'});
             const response = await request(server).get('/admins/1');
 
             expect(response.status).toBe(200);
         });
 
         it('responds with json type', async () => {
+            await request(server).post('/admins/register').send({username: 'username', password: 'password'});
             const response = await request(server).get('/admins/1');
 
             expect(response.type).toMatch(/json/i);
         });
 
         it('sends one admin by default', async () => {
-            request(server)
+            await request(server).post('/admins/register').send({username: 'username', password: 'password'});
             const response = await request(server).get('/admins/1');
 
-            expect(response.body).toEqual([]);
+            expect(response.body).toEqual( {"email": null, "firstName": null, "id": 1, "lastName": null, "password": "$2a$14$qoISKfveO9.FhKbCkSm9Jew370wn3aSTkwDl2C0uP9UZEYPiSVRDy", "type": null, "username": "username",});
         });
     });
 
     describe('post /admins/register', () => {
         it('responds with status code 201', async () => {
-            const body = { username: 'BennyBeneficiary', password: 'BennyPassword'}
-            const response = await request(server).post('/admins/register').send(body);
+            const response = await request(server).post('/admins/register').send({username: 'username', password: 'password'});
 
             expect(response.status).toBe(201);
         });
 
         it('responds with status code 422 when body is missing required data', async () => {
-            const body = { username: 'BennyBeneficiary' }
-            const response = await request(server).post('/admins/register').send(body);
+            const response = await request(server).post('/admins/register').send({username: 'username'});
 
             expect(response.status).toBe(422);
         });
 
         it('responds with an array containing a new id', async () => {
-            const body = { username: 'BennyBeneficiary', password: 'BennyPassword'}
-            const response = await request(server).post('/admins/register').send(body);
+            const response = await request(server).post('/admins/register').send({username: 'username', password: 'password'});
 
             expect(response.body).toEqual([1]);
         });
