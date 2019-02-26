661b1e0e41a3be05cfa435c9efe5c9e78fc47b3c
FireInjun <mk@fireinjun.com>
2019-02-26 08:08:38 -0500

    modified knex file for deployment testing

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,14 @@
 
 require('dotenv').config();
 
-const localPg = {
-  host: 'localhost',
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS
-}
+// const localPg = {
+//   host: 'localhost',
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS
+// }
 
-const dbConnection = process.env.DATABASE_URL || localPg;
+// const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
 
