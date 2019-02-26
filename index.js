982788f02c01823c28686902db5dd7504af59dde
FireInjun <mk@fireinjun.com>
2019-02-26 09:34:12 -0500

    Added CorsOptions for only allow app

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT
+const PORT = process.env.PORT
+const APP_URL = process.env.APP_URL
 const adminsRouter = require('./api/admins/adminsRouter.js');
 const donorsRouter = require('./api/donors/donorsRouter.js');
 const schoolsRouter = require('./api/schools/schoolsRouter.js');
 const donorViewRouter = require('./api/donorView/donorViewRouter.js');
 
 
-// const port = 4040; 
+const corsOptions = {
+    origin: APP_URL,
+    optionsSuccessStatus: 200
+}
+  
 const server = express();
 
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(express.json());
 
 server.use('/admins', adminsRouter);
@@ -19,6 +24,6 @@ server.use('/donors', donorsRouter);
 server.use('/schools', schoolsRouter);
 server.use('/donorView', donorViewRouter);
 
-server.listen(port);
+server.listen(PORT);
 
 module.exports = server;
